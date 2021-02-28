a4bd8579ed2d72f1f938b10a3498abeb7cda7b65
Sam Tait <samtait20@gmail.com>
2021-02-28 00:30:11 +0000

    Added Combo boxes for entering in the number of adults and children

--- cMain.cpp
+++ cMain.cpp
@@ -1,6 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <string>
-
 #include "cMain.h"
 
 wxBEGIN_EVENT_TABLE(cMain, wxFrame)
@@ -9,19 +7,37 @@ wxEND_EVENT_TABLE()
 
 cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Jumping Someone Else's Train", wxPoint(30, 30), wxSize(800, 600))
 {
-	
+
 	wxPanel* panel = new wxPanel(this, wxID_ANY);
 
 	// Options to be used for text box put into a wxArrayString
 	wxArrayString choices;
-	choices.Add("Adult");
-	choices.Add("Child");
+	choices.Add("Single");
+	choices.Add("Return");
 
 	// Create radio buttons
 	m_rad1 = new wxRadioBox(panel, 101, "Select one of the options", wxPoint(10, 10), wxDefaultSize, choices, 2, wxRA_VERTICAL);
 
 	// Temp text box to show selection
 	m_textctrl1 = new wxTextCtrl(panel, wxID_ANY, m_rad1->GetString(m_rad1->GetSelection()), wxPoint(10, 100), wxSize(140, wxDefaultCoord));
+
+	// Options for number of adults or children
+	wxArrayString comboChoices;
+	comboChoices.Add("0");
+	comboChoices.Add("1");
+	comboChoices.Add("2");
+	comboChoices.Add("3");
+	comboChoices.Add("4");
+	comboChoices.Add("5");
+
+	// Create both comboboxes for entering number of adults or children
+	m_adultcombo = new wxComboBox(panel, wxID_ANY, wxEmptyString, wxPoint(130, 140), wxDefaultSize, comboChoices, wxCB_DROPDOWN);
+	m_childcombo = new wxComboBox(panel, wxID_ANY, wxEmptyString, wxPoint(130, 170), wxDefaultSize, comboChoices, wxCB_DROPDOWN);
+
+	// Create text placed next to comboboxes that informs what info should be entered in each box
+	m_adulttext = new wxStaticText(panel, wxID_ANY, "Number of Adults:", wxPoint(10, 145), wxDefaultSize);
+	m_childtext = new wxStaticText(panel, wxID_ANY, "Number of Children:", wxPoint(10, 175), wxDefaultSize);
+
 }
 
 cMain::~cMain()
--- cMain.h
+++ cMain.h
@@ -14,6 +14,12 @@ public:
 	wxRadioBox* m_rad1 = nullptr;
 	wxTextCtrl* m_textctrl1 = nullptr;
 
+	wxComboBox* m_adultcombo = nullptr;
+	wxComboBox* m_childcombo = nullptr;
+	wxStaticText* m_adulttext = nullptr;
+	wxStaticText* m_childtext = nullptr;
+
+
 	wxDECLARE_EVENT_TABLE();
 };
 
